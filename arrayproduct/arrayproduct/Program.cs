3a6ff35f9a73f96b7769defba240e0476b8c2e9f
anoncipher404 <133158027+anoncipher404@users.noreply.github.com>
2023-09-26 06:18:52 +0530

    Add files via upload

---
+++ arrayproduct/arrayproduct/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace arrayproduct
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4 };
+            int m = 0;
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr.Length; j++)
+                {
+                    if (i != j)
+                    {
+                        m = m * arr[i];
+                    }
+
+                }
+
+            }
+            Console.WriteLine(m);
+        }
+    }
+}
